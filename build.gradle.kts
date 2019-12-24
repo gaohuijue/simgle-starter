644f60ed55c92efb016483031fbbad547c5812bd
gaohuijue <gaohuijue@shinow.com>
2019-12-24 16:52:44 +0800

    .

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    kotlin("jvm") version "1.3.61"
+    `maven-publish`
+}
+
+group = "com.simgle"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+    maven("http://maven.aliyun.com/nexus/content/groups/public/")
+    maven("http://maven.aliyun.com/nexus/content/repositories/jcenter")
+    mavenLocal()
+}
+
+dependencies {
+    implementation("com.simgle:simgle-core:+")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.2.2.RELEASE") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
---
+++ src/main/kotlin/com/simgle/starter/Application.kt
@@ -0,0 +1,8 @@
+package com.simgle.starter
+
+import com.simgle.core.SimgleApplication
+import org.springframework.boot.runApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimgleApplication>(*args)
+}
